5cd1e8dcd25f9759c2ea5f3d11ed9ffbb10b294e
Nur <nur-zaman-sazim@users.noreply.github.com>
2025-11-10 18:53:39 +0600

    feat: fix type errors

--- src/modules/vector-benchmarks/vector-benchmarks.controller.ts
+++ src/modules/vector-benchmarks/vector-benchmarks.controller.ts
@@ -1,24 +1,25 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
-import { VectorBenchmarksService } from './vector-benchmarks.service';
-import { BenchmarkConfig } from './interfaces/benchmark-result.interface';
+import { Controller, Post, Body, Get } from "@nestjs/common";
 
-@Controller('benchmarks')
+import { BenchmarkConfig } from "./interfaces/benchmark-result.interface";
+import { VectorBenchmarksService } from "./vector-benchmarks.service";
+
+@Controller("benchmarks")
 export class VectorBenchmarksController {
   constructor(private benchmarksService: VectorBenchmarksService) {}
 
-  @Get('databases')
+  @Get("databases")
   getRegisteredDatabases() {
     return {
       databases: this.benchmarksService.getRegisteredDatabases(),
     };
   }
 
-  @Post('run')
+  @Post("run")
   async runBenchmark(@Body() config: BenchmarkConfig) {
     return this.benchmarksService.runBenchmark(config);
   }
 
-  @Post('run-all')
+  @Post("run-all")
   async runAllBenchmarks(@Body() body: { vectorCount?: number }) {
     return this.benchmarksService.runAllBenchmarks(body.vectorCount);
   }
--- src/modules/vector-benchmarks/vector-benchmarks.module.ts
+++ src/modules/vector-benchmarks/vector-benchmarks.module.ts
@@ -1,15 +1,17 @@
-import { Module, OnModuleInit } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { VectorBenchmarksController } from './vector-benchmarks.controller';
-import { VectorBenchmarksService } from './vector-benchmarks.service';
-import { DataGeneratorModule } from './data-generators/data-generator.module';
-import { ReporterModule } from './reporters/reporter.module';
-import { LatencyBenchmark } from './benchmarks/latency.benchmark';
-import { PgVectorModule } from './databases/pgvector/pgvector.module';
-import { PgVectorService } from './databases/pgvector/pgvector.service';
-import { DatabaseType } from './vector-benchmarks.enums';
-import { BenchmarkResult } from '../../common/entities/benchmark-results.entity';
+import { Module, OnModuleInit } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+
+import { BenchmarkResult } from "../../common/entities/benchmark-results.entity";
+import { LatencyBenchmark } from "./benchmarks/latency.benchmark";
+import { DataGeneratorModule } from "./data-generators/data-generator.module";
+import { PgVectorModule } from "./databases/pgvector/pgvector.module";
+import { PgVectorService } from "./databases/pgvector/pgvector.service";
+import { ReporterModule } from "./reporters/reporter.module";
+import { VectorBenchmarksController } from "./vector-benchmarks.controller";
+import { DatabaseType } from "./vector-benchmarks.enums";
+import { VectorBenchmarksService } from "./vector-benchmarks.service";
 
 @Module({
   imports: [
@@ -31,9 +33,6 @@ export class VectorBenchmarksModule implements OnModuleInit {
 
   onModuleInit() {
     // Register database services
-    this.benchmarksService.registerDatabaseService(
-      DatabaseType.PGVECTOR,
-      this.pgVectorService,
-    );
+    this.benchmarksService.registerDatabaseService(DatabaseType.PGVECTOR, this.pgVectorService);
   }
 }
